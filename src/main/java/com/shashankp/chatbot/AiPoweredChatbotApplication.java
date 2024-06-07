a150e28624c1e29f52c221fce40cf1f8bff8a5f5
Shashank Pathipati <pshashank569@gmail.com>
2024-06-07 09:28:32 +0530

    initial commit

---
+++ src/main/java/com/shashankp/chatbot/AiPoweredChatbotApplication.java
@@ -0,0 +1,14 @@
+package com.shashankp.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPoweredChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPoweredChatbotApplication.class, args);
+		System.out.println("Hello Spring!");
+	}
+
+}
