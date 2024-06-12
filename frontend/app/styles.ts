94f15a2ad297f1ea60f88ed97a52e73bd087ad57
Shashank Pathipati <pshashank569@gmail.com>
2024-06-12 16:26:47 +0530

    frontend: added basic index page

---
+++ frontend/app/styles.ts
@@ -0,0 +1,10 @@
+import { styled } from "@mui/material";
+
+export const IndexMain = styled("main")`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  min-height: 100vh;
+`;
